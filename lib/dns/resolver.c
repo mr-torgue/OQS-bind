aa1b969db4d3e6e57601d35512f52a6616317ace
Dev <me+github@folmer.info>
2025-11-06 15:32:26 +1100

    dirty fix

--- lib/dns/resolver.c
+++ lib/dns/resolver.c
@@ -7336,9 +7336,10 @@ resquery_response(isc_result_t eresult, isc_region_t *region, void *arg) {
 		"7329!!!!!!!!!");
 
 	result = rctx_timedout(&rctx);
-	//if (result == ISC_R_COMPLETE) {
-	//		return;
-	//}
+	if (result == ISC_R_COMPLETE) {
+		//return;
+		result = ISC_R_SUCCESS; // dont do this kids!!
+	}
 
 	fctx->addrinfo = query->addrinfo;
 	fctx->timeout = false;
